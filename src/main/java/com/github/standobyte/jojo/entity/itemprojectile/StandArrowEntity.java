14d4c272acb8462da828a675e3008e6590605f16
StandoByte <94539965+StandoByte@users.noreply.github.com>
2021-11-28 19:08:27 +0300

    Fixed Stand Arrow entity not being summoned on client side

--- src/main/java/com/github/standobyte/jojo/entity/itemprojectile/StandArrowEntity.java
+++ src/main/java/com/github/standobyte/jojo/entity/itemprojectile/StandArrowEntity.java
@@ -9,7 +9,9 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.IPacket;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.network.NetworkHooks;
 
 public class StandArrowEntity extends AbstractArrowEntity {
     private ItemStack arrowItem = new ItemStack(ModItems.STAND_ARROW.get());
@@ -45,5 +47,10 @@ public class StandArrowEntity extends AbstractArrowEntity {
     public void addAdditionalSaveData(CompoundNBT arrow) {
         super.addAdditionalSaveData(arrow);
         arrow.put("Arrow", arrowItem.save(new CompoundNBT()));
+    }
+
+    @Override
+    public IPacket<?> getAddEntityPacket() {
+        return NetworkHooks.getEntitySpawningPacket(this);
     }
 }
