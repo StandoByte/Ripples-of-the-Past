b9bb9bbfc81497477da57e146a8ae41639165430
StandoByte <94539965+StandoByte@users.noreply.github.com>
2021-12-04 16:54:02 +0300

    Buffed vampire's freeze ability
    Made it freeze attackers, cancelling the attack
    Increased attribute modifiers

--- src/main/java/com/github/standobyte/jojo/action/actions/VampirismFreeze.java
+++ src/main/java/com/github/standobyte/jojo/action/actions/VampirismFreeze.java
@@ -3,10 +3,12 @@ package com.github.standobyte.jojo.action.actions;
 import com.github.standobyte.jojo.action.Action;
 import com.github.standobyte.jojo.action.ActionConditionResult;
 import com.github.standobyte.jojo.action.ActionTarget;
-import com.github.standobyte.jojo.client.sound.ClientTickingSoundsHelper;
+import com.github.standobyte.jojo.action.ActionTarget.TargetType;
+import com.github.standobyte.jojo.init.ModActions;
 import com.github.standobyte.jojo.init.ModEffects;
 import com.github.standobyte.jojo.init.ModSounds;
 import com.github.standobyte.jojo.power.IPower;
+import com.github.standobyte.jojo.power.nonstand.INonStandPower;
 import com.github.standobyte.jojo.util.damage.ModDamageSources;
 
 import net.minecraft.entity.Entity;
@@ -16,6 +18,7 @@ import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 
 public class VampirismFreeze extends Action {
 
@@ -43,23 +46,44 @@ public class VampirismFreeze extends Action {
     @Override
     public void onHoldTickUser(World world, LivingEntity user, IPower<?> power, int ticksHeld, ActionTarget target, boolean requirementsFulfilled) {
         if (!world.isClientSide() && requirementsFulfilled) {
-            Entity entityTarget = target.getEntity(world);
-            if (entityTarget instanceof LivingEntity) {
-                int difficulty = world.getDifficulty().getId();
-                LivingEntity targetLiving = (LivingEntity) entityTarget;
-                EffectInstance freezeInstance = targetLiving.getEffect(ModEffects.FREEZE.get());
-                if (freezeInstance == null) {
-                    targetLiving.addEffect(new EffectInstance(ModEffects.FREEZE.get(), difficulty * 30, 0));
+            if (target.getType() == TargetType.ENTITY) {
+                Entity entityTarget = target.getEntity(world);
+                if (entityTarget instanceof LivingEntity && !entityTarget.isOnFire()) {
+                    int difficulty = world.getDifficulty().getId();
+                    LivingEntity targetLiving = (LivingEntity) entityTarget;
+                    EffectInstance freezeInstance = targetLiving.getEffect(ModEffects.FREEZE.get());
+                    if (freezeInstance == null) {
+                        world.playSound(null, targetLiving, ModSounds.VAMPIRE_FREEZE.get(), targetLiving.getSoundSource(), 1.0F, 1.0F);
+                        targetLiving.addEffect(new EffectInstance(ModEffects.FREEZE.get(), difficulty * 30, 0));
+                    }
+                    else {
+                        int additionalDuration = 1 << difficulty;
+                        int duration = freezeInstance.getDuration() + additionalDuration;
+                        int lvl = duration / 120;
+                        targetLiving.addEffect(new EffectInstance(ModEffects.FREEZE.get(), duration, lvl));
+                    }
+                    ModDamageSources.dealColdDamage(targetLiving, 1.5F * difficulty, user, null);
                 }
-                else {
-                    int additionalDuration = 1 << difficulty;
-                    int duration = freezeInstance.getDuration() + additionalDuration;
-                    int lvl = duration / 120;
-                    targetLiving.addEffect(new EffectInstance(ModEffects.FREEZE.get(), duration, lvl));
-                }
-                ModDamageSources.dealColdDamage(targetLiving, 1.5F * difficulty, user, null);
             }
         }
+    }
+    
+    public static boolean onUserAttacked(LivingAttackEvent event) {
+        Entity attacker = event.getSource().getDirectEntity();
+        if (attacker instanceof LivingEntity && !attacker.isOnFire()) {
+            LivingEntity targetLiving = event.getEntityLiving();
+            return INonStandPower.getNonStandPowerOptional(targetLiving).map(power -> {
+                if (power.getHeldAction(true) == ModActions.VAMPIRISM_FREEZE.get()) {
+                    World world = attacker.level;
+                    int difficulty = world.getDifficulty().getId();
+                    ((LivingEntity) attacker).addEffect(new EffectInstance(ModEffects.FREEZE.get(), difficulty * 60, difficulty));
+                    world.playSound(null, attacker, ModSounds.VAMPIRE_FREEZE.get(), attacker.getSoundSource(), 1.0F, 1.0F);
+                    return true;
+                }
+                return false;
+            }).orElse(false);
+        }
+        return false;
     }
 
     @Override
@@ -70,13 +94,11 @@ public class VampirismFreeze extends Action {
     @Override
     public void onHoldTickClientEffect(LivingEntity user, IPower<?> power, int ticksHeld, boolean requirementsFulfilled, boolean stateRefreshed) {
         if (requirementsFulfilled) {
-            if (ticksHeld % 5 == 0) {
-                Vector3d particlePos = user.getEyePosition(1.0F).add(user.getForward().scale(0.75));
-                user.level.addParticle(ParticleTypes.CLOUD, particlePos.x, particlePos.y, particlePos.z, 0, 0, 0);
-            }
-            if (stateRefreshed) {
-                ClientTickingSoundsHelper.playHeldActionSound(ModSounds.VAMPIRE_FREEZE.get(), 1.0F, 1.0F, false, getPerformer(user, power), power, this);
-            }
+            Vector3d particlePos = user.position().add(
+                    (Math.random() - 0.5) * (user.getBbWidth() + 1.0), 
+                    Math.random() * (user.getBbHeight() + 1.0), 
+                    (Math.random() - 0.5) * (user.getBbWidth() + 1.0));
+            user.level.addParticle(ParticleTypes.CLOUD, particlePos.x, particlePos.y, particlePos.z, 0, 0, 0);
         }
     }
 }
