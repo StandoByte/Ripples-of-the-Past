097496d02f1eb785d8e2592969f8b486decd6e3a
StandoByte <94539965+StandoByte@users.noreply.github.com>
2022-06-23 00:59:30 +0300

    Made tommy gun attributes map lazily initialize

--- src/main/java/com/github/standobyte/jojo/init/ModEntityAttributes.java
+++ src/main/java/com/github/standobyte/jojo/init/ModEntityAttributes.java
@@ -4,9 +4,6 @@ import com.github.standobyte.jojo.JojoMod;
 
 import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.RangedAttribute;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.registries.DeferredRegister;
@@ -21,11 +18,4 @@ public class ModEntityAttributes {
 
     public static final RegistryObject<Attribute> STAND_PRECISION = ATTRIBUTES.register("stand_precision", 
             () -> new RangedAttribute("attribute.name.generic.max_health", 0.0, 0.0, 160.0).setSyncable(true));
-    
-    
-    
-    @SubscribeEvent(priority = EventPriority.LOW)
-    public static final void afterAttributesRegister(RegistryEvent.Register<Attribute> event) {
-        ModItems.TOMMY_GUN.get().initAttributeModifiers();
-    }
 }
