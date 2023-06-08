1c872bf40ac698ac340dd45a67460968e22ab87a
StandoByte <94539965+StandoByte@users.noreply.github.com>
2023-06-08 21:49:10 +0300

    Made client reflection fields final
    Should still be safe on dedicated servers, and this way they'll be a bit
    more optimized

--- src/main/java/com/github/standobyte/jojo/util/mc/reflection/ClientReflection.java
+++ src/main/java/com/github/standobyte/jojo/util/mc/reflection/ClientReflection.java
@@ -32,53 +32,21 @@ import net.minecraft.client.shader.ShaderGroup;
 import net.minecraft.util.HandSide;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Timer;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 public class ClientReflection {
-    static {
-        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> new DistExecutor.SafeRunnable() {
-            
-            @Override
-            public void run() {
-                FIRST_PERSON_RENDERER_OFF_HAND_HEIGHT = ObfuscationReflectionHelper.findField(FirstPersonRenderer.class, "field_187471_h");
-                FIRST_PERSON_RENDERER_O_OFF_HAND_HEIGHT = ObfuscationReflectionHelper.findField(FirstPersonRenderer.class, "field_187472_i");
-                FIRST_PERSON_RENDERER_RENDER_PLAYER_ARM = ObfuscationReflectionHelper.findMethod(FirstPersonRenderer.class, "func_228401_a_", 
-                        MatrixStack.class, IRenderTypeBuffer.class, int.class, float.class, float.class, HandSide.class);
-                MINECRAFT_PAUSE = ObfuscationReflectionHelper.findField(Minecraft.class, "field_71445_n");
-                MINECRAFT_TIMER = ObfuscationReflectionHelper.findField(Minecraft.class, "field_71428_T");
-                MAIN_MENU_SCREEN_SPLASH = ObfuscationReflectionHelper.findField(MainMenuScreen.class, "field_73975_c");
-                RENDER_TYPE_BUFFER_IMPL_BUILDER = ObfuscationReflectionHelper.findField(IRenderTypeBuffer.Impl.class, "field_228457_a_");
-                RENDER_TYPE_BUFFER_IMPL_FIXED_BUFFERS = ObfuscationReflectionHelper.findField(IRenderTypeBuffer.Impl.class, "field_228458_b_");
-                MODEL_RENDERER_CUBES = ObfuscationReflectionHelper.findField(ModelRenderer.class, "field_78804_l");
-                MODEL_BOX_POLYGONS = ObfuscationReflectionHelper.findField(ModelRenderer.ModelBox.class, "field_78254_i");
-                SOUND_EVENT_ACCESSOR_LIST = ObfuscationReflectionHelper.findField(SoundEventAccessor.class, "field_188716_a");
-                INGAME_MENU_SCREEN_SHOW_PAUSE_MENU = ObfuscationReflectionHelper.findField(IngameMenuScreen.class, "field_222813_a");
-                PARTICLE_MANAGER_SPRITE_SETS = ObfuscationReflectionHelper.findField(ParticleManager.class, "field_215242_i");
-                MODEL_BAKERY_UNREFERENCED_TEXTURES = ObfuscationReflectionHelper.findField(ModelBakery.class, "field_177602_b");
-                TEXTURE_MANAGER_TICKABLE_TEXTURES = ObfuscationReflectionHelper.findField(TextureManager.class, "field_110583_b");
-                MINECRAFT_PARTICLE_ENGINE = ObfuscationReflectionHelper.findField(Minecraft.class, "field_71452_i");
-                PARTICLE_MANAGER_TEXTURE_MANAGER = ObfuscationReflectionHelper.findField(ParticleManager.class, "field_78877_c");
-                SHADER_GROUP_PASSES = ObfuscationReflectionHelper.findField(ShaderGroup.class, "field_148031_d");
-                GAME_RENDERER_POST_EFFECT = ObfuscationReflectionHelper.findField(GameRenderer.class, "field_147707_d");
-                GAME_RENDERER_EFFECT_ACTIVE = ObfuscationReflectionHelper.findField(GameRenderer.class, "field_175083_ad");
-                GAME_RENDERER_EFFECT_INDEX = ObfuscationReflectionHelper.findField(GameRenderer.class, "field_147713_ae");
-            }
-        });
-    }
-    
-    private static Field FIRST_PERSON_RENDERER_OFF_HAND_HEIGHT;
+    private static final Field FIRST_PERSON_RENDERER_OFF_HAND_HEIGHT = ObfuscationReflectionHelper.findField(FirstPersonRenderer.class, "field_187471_h");
     public static float getOffHandHeight(FirstPersonRenderer renderer) {
         return ReflectionUtil.getFieldValue(FIRST_PERSON_RENDERER_OFF_HAND_HEIGHT, renderer);
     }
 
-    private static Field FIRST_PERSON_RENDERER_O_OFF_HAND_HEIGHT;
+    private static final Field FIRST_PERSON_RENDERER_O_OFF_HAND_HEIGHT = ObfuscationReflectionHelper.findField(FirstPersonRenderer.class, "field_187472_i");
     public static float getOffHandHeightPrev(FirstPersonRenderer renderer) {
         return ReflectionUtil.getFieldValue(FIRST_PERSON_RENDERER_O_OFF_HAND_HEIGHT, renderer);
     }
 
-    private static Method FIRST_PERSON_RENDERER_RENDER_PLAYER_ARM;
+    private static final Method FIRST_PERSON_RENDERER_RENDER_PLAYER_ARM = ObfuscationReflectionHelper.findMethod(FirstPersonRenderer.class, "func_228401_a_", 
+            MatrixStack.class, IRenderTypeBuffer.class, int.class, float.class, float.class, HandSide.class);
     public static void renderPlayerArm(MatrixStack matrixStack, IRenderTypeBuffer buffer, int packedLight, 
             float handHeight, float swingAnim, HandSide handSide, FirstPersonRenderer renderer) {
         ReflectionUtil.invokeMethod(FIRST_PERSON_RENDERER_RENDER_PLAYER_ARM, 
@@ -86,35 +54,35 @@ public class ClientReflection {
     }
     
     
-    private static Field MINECRAFT_PAUSE;
+    private static final Field MINECRAFT_PAUSE = ObfuscationReflectionHelper.findField(Minecraft.class, "field_71445_n");
     public static void pauseClient(Minecraft minecraft) {
         ReflectionUtil.setFieldValue(MINECRAFT_PAUSE, minecraft, true);
     }
 
-    private static Field MINECRAFT_TIMER;
+    private static final Field MINECRAFT_TIMER = ObfuscationReflectionHelper.findField(Minecraft.class, "field_71428_T");
     public static Timer getTimer(Minecraft minecraft) {
         return ReflectionUtil.getFieldValue(MINECRAFT_TIMER, minecraft);
     }
     
     
-    private static Field MAIN_MENU_SCREEN_SPLASH;
+    private static final Field MAIN_MENU_SCREEN_SPLASH = ObfuscationReflectionHelper.findField(MainMenuScreen.class, "field_73975_c");
     public static void setSplash(MainMenuScreen screen, String splash) {
         ReflectionUtil.setFieldValue(MAIN_MENU_SCREEN_SPLASH, screen, splash);
     }
     
     
-    private static Field RENDER_TYPE_BUFFER_IMPL_BUILDER;
+    private static final Field RENDER_TYPE_BUFFER_IMPL_BUILDER = ObfuscationReflectionHelper.findField(IRenderTypeBuffer.Impl.class, "field_228457_a_");
     public static BufferBuilder getBuilder(IRenderTypeBuffer.Impl buffers) {
         return ReflectionUtil.getFieldValue(RENDER_TYPE_BUFFER_IMPL_BUILDER, buffers);
     }
     
-    private static Field RENDER_TYPE_BUFFER_IMPL_FIXED_BUFFERS;
+    private static final Field RENDER_TYPE_BUFFER_IMPL_FIXED_BUFFERS = ObfuscationReflectionHelper.findField(IRenderTypeBuffer.Impl.class, "field_228458_b_");
     public static Map<RenderType, BufferBuilder> getFixedBuffers(IRenderTypeBuffer.Impl buffers) {
         return ReflectionUtil.getFieldValue(RENDER_TYPE_BUFFER_IMPL_FIXED_BUFFERS, buffers);
     }
     
     
-    private static Field MODEL_RENDERER_CUBES;
+    private static final Field MODEL_RENDERER_CUBES = ObfuscationReflectionHelper.findField(ModelRenderer.class, "field_78804_l");
     public static void setCubes(ModelRenderer modelRenderer, ObjectList<ModelRenderer.ModelBox> cubes) {
         ReflectionUtil.setFieldValue(MODEL_RENDERER_CUBES, modelRenderer, cubes);
     }
@@ -125,7 +93,7 @@ public class ClientReflection {
     }
     
     
-    private static Field MODEL_BOX_POLYGONS;
+    private static final Field MODEL_BOX_POLYGONS = ObfuscationReflectionHelper.findField(ModelRenderer.ModelBox.class, "field_78254_i");
     @Deprecated
     public static ModelRenderer.TexturedQuad[] getPolygons(ModelRenderer.ModelBox modelBox) {
         return ReflectionUtil.getFieldValue(MODEL_BOX_POLYGONS, modelBox);
@@ -136,31 +104,31 @@ public class ClientReflection {
     }
     
     
-    private static Field SOUND_EVENT_ACCESSOR_LIST;
+    private static final Field SOUND_EVENT_ACCESSOR_LIST = ObfuscationReflectionHelper.findField(SoundEventAccessor.class, "field_188716_a");
     public static List<ISoundEventAccessor<Sound>> getSubAccessorsList(SoundEventAccessor accessor) {
         return ReflectionUtil.getFieldValue(SOUND_EVENT_ACCESSOR_LIST, accessor);
     }
     
     
-    private static Field INGAME_MENU_SCREEN_SHOW_PAUSE_MENU;
+    private static final Field INGAME_MENU_SCREEN_SHOW_PAUSE_MENU = ObfuscationReflectionHelper.findField(IngameMenuScreen.class, "field_222813_a");
     public static boolean showsPauseMenu(IngameMenuScreen screen) {
         return ReflectionUtil.getFieldValue(INGAME_MENU_SCREEN_SHOW_PAUSE_MENU, screen);
     }
     
     
-    private static Field PARTICLE_MANAGER_SPRITE_SETS;
+    private static final Field PARTICLE_MANAGER_SPRITE_SETS = ObfuscationReflectionHelper.findField(ParticleManager.class, "field_215242_i");
     public static Map<ResourceLocation, ? extends IAnimatedSprite> getSpriteSets(ParticleManager particleManager) {
         return ReflectionUtil.getFieldValue(PARTICLE_MANAGER_SPRITE_SETS, particleManager);
     }
     
     
-    private static Field MODEL_BAKERY_UNREFERENCED_TEXTURES;
+    private static final Field MODEL_BAKERY_UNREFERENCED_TEXTURES = ObfuscationReflectionHelper.findField(ModelBakery.class, "field_177602_b");
     public static Set<RenderMaterial> getModelBakeryUnreferencedTextures() {
         return ReflectionUtil.getFieldValue(MODEL_BAKERY_UNREFERENCED_TEXTURES, null);
     }
 
     
-    private static Field TEXTURE_MANAGER_TICKABLE_TEXTURES;
+    private static final Field TEXTURE_MANAGER_TICKABLE_TEXTURES = ObfuscationReflectionHelper.findField(TextureManager.class, "field_110583_b");
     public static Set<ITickable> getTickableTextures(TextureManager textureManager) {
         return ReflectionUtil.getFieldValue(TEXTURE_MANAGER_TICKABLE_TEXTURES, textureManager);
     }
@@ -170,33 +138,33 @@ public class ClientReflection {
     }
     
     
-    private static Field MINECRAFT_PARTICLE_ENGINE;
+    private static final Field MINECRAFT_PARTICLE_ENGINE = ObfuscationReflectionHelper.findField(Minecraft.class, "field_71452_i");
     public static void setParticleEngine(Minecraft mc, ParticleManager particleEngine) {
         ReflectionUtil.setFieldValue(MINECRAFT_PARTICLE_ENGINE, mc, particleEngine);
     }
     
-    private static Field PARTICLE_MANAGER_TEXTURE_MANAGER;
+    private static final Field PARTICLE_MANAGER_TEXTURE_MANAGER = ObfuscationReflectionHelper.findField(ParticleManager.class, "field_78877_c");
     public static void setTextureManager(ParticleManager particleManager, TextureManager textureManager) {
         ReflectionUtil.setFieldValue(PARTICLE_MANAGER_TEXTURE_MANAGER, particleManager, textureManager);
     }
     
     
-    private static Field SHADER_GROUP_PASSES;
+    private static final Field SHADER_GROUP_PASSES = ObfuscationReflectionHelper.findField(ShaderGroup.class, "field_148031_d");
     public static List<Shader> getShaderGroupPasses(ShaderGroup shaderGroup) {
         return ReflectionUtil.getFieldValue(SHADER_GROUP_PASSES, shaderGroup);
     }
     
-    private static Field GAME_RENDERER_POST_EFFECT;
+    private static final Field GAME_RENDERER_POST_EFFECT = ObfuscationReflectionHelper.findField(GameRenderer.class, "field_147707_d");
     public static void setPostEffect(GameRenderer gameRenderer, ShaderGroup postEffect) {
         ReflectionUtil.setFieldValue(GAME_RENDERER_POST_EFFECT, gameRenderer, postEffect);
     }
     
-    private static Field GAME_RENDERER_EFFECT_ACTIVE;
+    private static final Field GAME_RENDERER_EFFECT_ACTIVE = ObfuscationReflectionHelper.findField(GameRenderer.class, "field_175083_ad");
     public static void setEffectActive(GameRenderer gameRenderer, boolean effectActive) {
         ReflectionUtil.setFieldValue(GAME_RENDERER_EFFECT_ACTIVE, gameRenderer, effectActive);
     }
     
-    private static Field GAME_RENDERER_EFFECT_INDEX;
+    private static final Field GAME_RENDERER_EFFECT_INDEX = ObfuscationReflectionHelper.findField(GameRenderer.class, "field_147713_ae");
     public static void setEffectIndex(GameRenderer gameRenderer, int effectIndex) {
         ReflectionUtil.setFieldValue(GAME_RENDERER_EFFECT_INDEX, gameRenderer, effectIndex);
     }
