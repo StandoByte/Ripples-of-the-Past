45cdb847afda4adb82b2af62d1b4b21c87a2c02e
StandoByte <94539965+StandoByte@users.noreply.github.com>
2023-06-17 19:39:13 +0300

    Hamon spread disrupting phantoms flight

--- src/main/java/com/github/standobyte/jojo/potion/HamonSpreadEffect.java
+++ src/main/java/com/github/standobyte/jojo/potion/HamonSpreadEffect.java
@@ -1,10 +1,12 @@
 package com.github.standobyte.jojo.potion;
 
-import com.github.standobyte.jojo.util.mc.damage.DamageUtil;
 import com.github.standobyte.jojo.util.mod.JojoModUtil;
 
+import net.minecraft.entity.FlyingEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.potion.EffectType;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraftforge.common.ForgeMod;
 
 public class HamonSpreadEffect extends UncurableEffect implements IApplicableEffect {
 
@@ -14,17 +16,23 @@ public class HamonSpreadEffect extends UncurableEffect implements IApplicableEff
     
     @Override
     public void applyEffectTick(LivingEntity livingEntity, int amplifier) {
-        DamageUtil.dealHamonDamage(livingEntity, 1F, null, null);
+//        DamageUtil.dealHamonDamage(livingEntity, 1F, null, null);
+        if (!livingEntity.level.isClientSide() && livingEntity instanceof FlyingEntity) {
+            double gravity = livingEntity.getAttributeValue(ForgeMod.ENTITY_GRAVITY.get());
+            Vector3d deltaMovement = livingEntity.getDeltaMovement();
+            livingEntity.setDeltaMovement(new Vector3d(deltaMovement.x, -gravity * (amplifier + 1) * 0.5, deltaMovement.z));
+        }
     }
 
     @Override
     public boolean isDurationEffectTick(int duration, int amplifier) {
-        int j = 25 >> amplifier;
-        if (j > 0) {
-            return duration % j == 0;
-        } else {
-            return true;
-        }
+//        int j = 25 >> amplifier;
+//        if (j > 0) {
+//            return duration % j == 0;
+//        } else {
+//            return true;
+//        }
+        return true;
     }
 
     @Override
