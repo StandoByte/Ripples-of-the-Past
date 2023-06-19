4dbaa974145573a67e1de04ac2f5afda4a48d316
StandoByte <94539965+StandoByte@users.noreply.github.com>
2023-06-19 16:31:13 +0300

    Disabled Pluck voice line for now

--- src/main/java/com/github/standobyte/jojo/action/non_stand/HamonMetalSilverOverdrive.java
+++ src/main/java/com/github/standobyte/jojo/action/non_stand/HamonMetalSilverOverdrive.java
@@ -1,20 +1,14 @@
 package com.github.standobyte.jojo.action.non_stand;
 
-import javax.annotation.Nullable;
-
 import com.github.standobyte.jojo.action.ActionConditionResult;
-import com.github.standobyte.jojo.action.ActionTarget;
 import com.github.standobyte.jojo.init.ModParticles;
-import com.github.standobyte.jojo.init.ModSounds;
 import com.github.standobyte.jojo.power.impl.nonstand.INonStandPower;
 import com.github.standobyte.jojo.power.impl.nonstand.type.hamon.HamonData;
 import com.github.standobyte.jojo.util.mc.damage.DamageUtil;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.SwordItem;
 import net.minecraft.item.TieredItem;
-import net.minecraft.util.SoundEvent;
 
 public class HamonMetalSilverOverdrive extends HamonOverdrive {
 
@@ -32,17 +26,17 @@ public class HamonMetalSilverOverdrive extends HamonOverdrive {
         return !heldItemStack.isEmpty() && heldItemStack.getItem() instanceof TieredItem;
     }
 
-    @Override
-    @Nullable
-    protected SoundEvent getShout(LivingEntity user, INonStandPower power, ActionTarget target, boolean wasActive) {
-        ItemStack heldItemStack = user.getMainHandItem();
-        if (heldItemStack.getItem() instanceof SwordItem
-                && heldItemStack.hasCustomHoverName()
-                && "pluck".equals(heldItemStack.getHoverName().getString().toLowerCase())) {
-            return ModSounds.JONATHAN_PLUCK_SWORD.get();
-        }
-        return null;
-    }
+//    @Override
+//    @Nullable
+//    protected SoundEvent getShout(LivingEntity user, INonStandPower power, ActionTarget target, boolean wasActive) {
+//        ItemStack heldItemStack = user.getMainHandItem();
+//        if (heldItemStack.getItem() instanceof SwordItem
+//                && heldItemStack.hasCustomHoverName()
+//                && "pluck".equals(heldItemStack.getHoverName().getString().toLowerCase())) {
+//            return ModSounds.JONATHAN_PLUCK_SWORD.get();
+//        }
+//        return null;
+//    }
     
     @Override
     protected boolean dealDamage(LivingEntity target, float dmgAmount, LivingEntity user, INonStandPower power, HamonData hamon) {
