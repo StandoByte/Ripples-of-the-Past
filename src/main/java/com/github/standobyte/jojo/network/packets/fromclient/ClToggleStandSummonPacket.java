4503c3e96e2e523a4dea661490d4a49c8b39508d
StandoByte <94539965+StandoByte@users.noreply.github.com>
2024-01-09 00:17:39 +0300

    Saves missing Stand instance and unlocked actions into NBT
    at least it should, gotta make the commit first to test

--- src/main/java/com/github/standobyte/jojo/network/packets/fromclient/ClToggleStandSummonPacket.java
+++ src/main/java/com/github/standobyte/jojo/network/packets/fromclient/ClToggleStandSummonPacket.java
@@ -7,7 +7,6 @@ import com.github.standobyte.jojo.power.impl.stand.IStandPower;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.fml.network.NetworkEvent;
 
 public class ClToggleStandSummonPacket {
@@ -29,12 +28,7 @@ public class ClToggleStandSummonPacket {
             PlayerEntity player = ctx.get().getSender();
             if (player.isAlive()) {
                 IStandPower.getStandPowerOptional(player).ifPresent(power -> {
-                    if (power.hasPower()) {
-                        power.toggleSummon();
-                    }
-                    else {
-                        player.displayClientMessage(new TranslationTextComponent("jojo.chat.message.no_stand"), true);
-                    }
+                    power.toggleSummon();
                 });
             }
         }
