42bc141b208485a71e7719f8df745acb3e4b52cb
StandoByte <94539965+StandoByte@users.noreply.github.com>
2024-03-02 01:11:30 +0300

    Mixin: tracking items in chest/hopper minecarts

---
+++ src/main/java/com/github/standobyte/jojo/mixin/itemtracking/ContainerMinecartEntityMixin.java
@@ -0,0 +1,33 @@
+package com.github.standobyte.jojo.mixin.itemtracking;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.github.standobyte.jojo.itemtracking.itemcap.TrackerItemStack;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.item.minecart.AbstractMinecartEntity;
+import net.minecraft.entity.item.minecart.ContainerMinecartEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraft.world.World;
+
+@Mixin(ContainerMinecartEntity.class)
+public abstract class ContainerMinecartEntityMixin extends AbstractMinecartEntity {
+    
+    protected ContainerMinecartEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+    
+    @Shadow
+    private NonNullList<ItemStack> itemStacks;
+    
+    @Inject(method = "setItem", at = @At("TAIL"))
+    public void jojoOnItemSetToSlot(int slot, ItemStack item, CallbackInfo ci) {
+        TrackerItemStack.trackedInEntityInv(item, itemStacks.stream(), 
+                level, getId());
+    }
+}
