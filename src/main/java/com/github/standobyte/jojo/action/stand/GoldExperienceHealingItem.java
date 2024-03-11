5df47f774e927de4df2c500252200b3ea0acb6cc
StandoByte <94539965+StandoByte@users.noreply.github.com>
2024-03-11 15:53:01 +0300

    Fixed some sounds not playing when using GE's healing

--- src/main/java/com/github/standobyte/jojo/action/stand/GoldExperienceHealingItem.java
+++ src/main/java/com/github/standobyte/jojo/action/stand/GoldExperienceHealingItem.java
@@ -5,14 +5,17 @@ import com.github.standobyte.jojo.action.ActionTarget;
 import com.github.standobyte.jojo.entity.stand.StandEntity;
 import com.github.standobyte.jojo.entity.stand.StandEntityTask;
 import com.github.standobyte.jojo.init.ModItems;
+import com.github.standobyte.jojo.init.ModSounds;
 import com.github.standobyte.jojo.item.GEBodyTissueItem;
 import com.github.standobyte.jojo.power.impl.stand.IStandPower;
+import com.github.standobyte.jojo.power.impl.stand.StandUtil;
 import com.github.standobyte.jojo.util.mc.MCUtil;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BucketItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 
 public class GoldExperienceHealingItem extends StandEntityAction {
@@ -50,6 +53,9 @@ public class GoldExperienceHealingItem extends StandEntityAction {
             ItemStack tissueItem = new ItemStack(ModItems.GOLD_EXPERIENCE_BODY_TISSUE.get());
             GEBodyTissueItem.onCreated(userPower, tissueItem);
             MCUtil.giveItemTo(user, tissueItem, false);
+            
+            MCUtil.playSound(user.level, null, user, ModSounds.GOLD_EXPERIENCE_LIFE_START.get(), 
+                    SoundCategory.AMBIENT, 1.0F, 0.95F + user.getRandom().nextFloat() * 0.1F, StandUtil::playerCanHearStands);
         }
     }
     
