7e0b3e865968c9a96e21a972ecd794d3e83ebcba
StandoByte <94539965+StandoByte@users.noreply.github.com>
2024-03-21 21:58:34 +0300

    Updated client only glowing for GE Life detector

--- src/main/java/com/github/standobyte/jojo/action/stand/GoldExperienceLifeDetector.java
+++ src/main/java/com/github/standobyte/jojo/action/stand/GoldExperienceLifeDetector.java
@@ -1,15 +1,17 @@
 package com.github.standobyte.jojo.action.stand;
 
 import java.util.List;
+import java.util.OptionalInt;
 
+import com.github.standobyte.jojo.capability.entity.EntityUtilCapProvider;
+import com.github.standobyte.jojo.client.ClientUtil;
+import com.github.standobyte.jojo.client.ui.actionshud.ActionsOverlayGui;
 import com.github.standobyte.jojo.entity.stand.StandEntity;
 import com.github.standobyte.jojo.entity.stand.StandEntityTask;
 import com.github.standobyte.jojo.power.impl.stand.IStandPower;
 import com.github.standobyte.jojo.util.mc.MCUtil;
 
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
 import net.minecraft.world.World;
 
 public class GoldExperienceLifeDetector extends StandEntityAction {
@@ -21,13 +23,14 @@ public class GoldExperienceLifeDetector extends StandEntityAction {
     @Override
     public void standTickPerform(World world, StandEntity standEntity, IStandPower userPower, StandEntityTask task) {
         int tick = task.getTick();
-        if (!world.isClientSide()) {
+        if (world.isClientSide() && userPower.getUser() == ClientUtil.getClientPlayer()) {
             double radius = (double) tick;
             double maxRadius = 32;
             List<LivingEntity> entitiesAround = MCUtil.entitiesAround(LivingEntity.class, standEntity, 
                     Math.min(radius, maxRadius), false, 
                     entity -> entity != userPower.getUser() && GoldExperienceHeal.isLiving(entity));
-            entitiesAround.forEach(entity -> entity.addEffect(new EffectInstance(Effects.GLOWING, 80)));
+            entitiesAround.forEach(entity -> entity.getCapability(EntityUtilCapProvider.CAPABILITY).ifPresent(
+                    cap -> cap.setClGlowingColor(OptionalInt.of(ActionsOverlayGui.getPowerUiColor(userPower)), 80)));
         }
     }
 
