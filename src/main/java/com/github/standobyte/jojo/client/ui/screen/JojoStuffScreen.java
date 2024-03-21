16775ebee18fa8a35aef635c2fc85dcabeb1ec21
StandoByte <94539965+StandoByte@users.noreply.github.com>
2024-03-21 21:47:19 +0300

    Disabled Stand skins screen

--- src/main/java/com/github/standobyte/jojo/client/ui/screen/JojoStuffScreen.java
+++ src/main/java/com/github/standobyte/jojo/client/ui/screen/JojoStuffScreen.java
@@ -157,7 +157,9 @@ public class JojoStuffScreen {
                 StandInfoScreen::new).disable() /* WIP screen */),
         CONTROLS(CONTROLS_TAB),
         SKINS(new Tab(null, 0, 0, 16, 16, new TranslationTextComponent("jojo.stand_skins.button"), 
-                () -> new StandSkinsScreen(IStandPower.getPlayerStandPower(Minecraft.getInstance().player))));
+                () -> new StandSkinsScreen(IStandPower.getPlayerStandPower(Minecraft.getInstance().player)))
+                .disable() /* WIP */
+                );
         
         private final Tab tab;
         private StandTab(Tab tab) {
