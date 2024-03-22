53fdbac384cf14bda16583561612a4d5b892d15d
StandoByte <94539965+StandoByte@users.noreply.github.com>
2024-03-22 13:11:34 +0300

    Made GE life detector more accurate detecting souls

--- src/main/java/com/github/standobyte/jojo/action/stand/GoldExperienceLifeDetector.java
+++ src/main/java/com/github/standobyte/jojo/action/stand/GoldExperienceLifeDetector.java
@@ -1,16 +1,19 @@
 package com.github.standobyte.jojo.action.stand;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalInt;
 
 import com.github.standobyte.jojo.capability.entity.EntityUtilCapProvider;
 import com.github.standobyte.jojo.client.ClientUtil;
 import com.github.standobyte.jojo.client.ui.actionshud.ActionsOverlayGui;
+import com.github.standobyte.jojo.entity.SoulEntity;
 import com.github.standobyte.jojo.entity.stand.StandEntity;
 import com.github.standobyte.jojo.entity.stand.StandEntityTask;
 import com.github.standobyte.jojo.power.impl.stand.IStandPower;
 import com.github.standobyte.jojo.util.mc.MCUtil;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.world.World;
 
@@ -24,18 +27,23 @@ public class GoldExperienceLifeDetector extends StandEntityAction {
     public void standTickPerform(World world, StandEntity standEntity, IStandPower userPower, StandEntityTask task) {
         int tick = task.getTick();
         if (world.isClientSide() && userPower.getUser() == ClientUtil.getClientPlayer()) {
-            double radius = (double) tick;
-            double maxRadius = 32;
-            List<LivingEntity> entitiesAround = MCUtil.entitiesAround(LivingEntity.class, standEntity, 
-                    Math.min(radius, maxRadius), false, 
-                    entity -> entity != userPower.getUser() && GoldExperienceHeal.isLiving(entity));
+            double radius = Math.min((double) tick, 32);
+            List<Entity> entitiesAround = new ArrayList<>();
+            
+            entitiesAround.addAll(MCUtil.entitiesAround(LivingEntity.class, standEntity, 
+                    radius, false, 
+                    entity -> entity != userPower.getUser() && GoldExperienceHeal.isLiving(entity)));
+            entitiesAround.addAll(MCUtil.entitiesAround(SoulEntity.class, standEntity,
+                    radius, false,
+                    null));
+            
             entitiesAround.forEach(entity -> entity.getCapability(EntityUtilCapProvider.CAPABILITY).ifPresent(
                     cap -> {
-                        if (!entity.isDeadOrDying()) {
-                            cap.setClGlowingColor(OptionalInt.of(ActionsOverlayGui.getPowerUiColor(userPower)), 80);
+                        if (entity instanceof LivingEntity && ((LivingEntity) entity).isDeadOrDying()) {
+                            cap.setClGlowingColor(OptionalInt.empty(), 80);
                         }
                         else {
-                            cap.setClGlowingColor(OptionalInt.empty(), 80);
+                            cap.setClGlowingColor(OptionalInt.of(ActionsOverlayGui.getPowerUiColor(userPower)), 80);
                         }
                     }));
         }
