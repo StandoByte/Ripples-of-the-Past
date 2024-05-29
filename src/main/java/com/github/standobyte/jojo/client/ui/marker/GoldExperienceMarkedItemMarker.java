77d11194f03ea0a05d2ac8a5540785c2c0c33567
StandoByte <94539965+StandoByte@users.noreply.github.com>
2024-05-29 22:05:13 +0300

    Using the same method to render tracked item marker

--- src/main/java/com/github/standobyte/jojo/client/ui/marker/GoldExperienceMarkedItemMarker.java
+++ src/main/java/com/github/standobyte/jojo/client/ui/marker/GoldExperienceMarkedItemMarker.java
@@ -7,21 +7,11 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import com.github.standobyte.jojo.action.stand.GoldExperienceMarkItem;
 import com.github.standobyte.jojo.action.stand.effect.GEItemMarkEffect;
-import com.github.standobyte.jojo.client.ClientUtil;
 import com.github.standobyte.jojo.itemtracking.itemcap.TrackerItemStack;
 import com.github.standobyte.jojo.power.impl.stand.IStandPower;
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.client.renderer.model.IBakedModel;
-import net.minecraft.client.renderer.model.ItemCameraTransforms;
-import net.minecraft.client.renderer.texture.AtlasTexture;
-import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.vector.Vector3d;
 
@@ -36,39 +26,11 @@ public class GoldExperienceMarkedItemMarker extends MarkerRenderer {
         return true;
     }
     
-    @SuppressWarnings("deprecation")
     @Override
     protected void renderIcon(MatrixStack matrixStack, MarkerInstance marker, float partialTick) {
         ItemStack item = ((ItemMarkerInstance) marker).item;
         if (item != null && !item.isEmpty()) {
-            ItemRenderer itemRenderer = mc.getItemRenderer();
-            
-            TextureManager textureManager = mc.textureManager;
-            IBakedModel itemModel = itemRenderer.getModel(item, null, null);
-
-            textureManager.bind(AtlasTexture.LOCATION_BLOCKS);
-            textureManager.getTexture(AtlasTexture.LOCATION_BLOCKS).setFilter(false, false);
-            RenderSystem.enableRescaleNormal();
-            RenderSystem.enableAlphaTest();
-            RenderSystem.defaultAlphaFunc();
-            RenderSystem.enableBlend();
-            RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-            RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-            IRenderTypeBuffer.Impl buffer = mc.renderBuffers().bufferSource();
-
-            matrixStack.pushPose();
-            matrixStack.translate(8, 8, 0);
-            matrixStack.scale(16, 16, 0.0625f);
-            matrixStack.scale(1, -1, -1);
-
-            itemRenderer.render(item, ItemCameraTransforms.TransformType.GUI, false, 
-                    matrixStack, buffer, ClientUtil.MAX_MODEL_LIGHT, OverlayTexture.NO_OVERLAY, itemModel);
-            matrixStack.popPose();
-            buffer.endBatch();
-            RenderSystem.enableDepthTest();
-
-            RenderSystem.disableAlphaTest();
-            RenderSystem.disableRescaleNormal();
+            renderItem(matrixStack, item, partialTick);
         }
     }
     
