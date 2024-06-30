f573876029acd82896b1ff818ab400700b6a5b32
StandoByte <94539965+StandoByte@users.noreply.github.com>
2024-06-30 16:29:48 +0300

    Highlight item only when hovering over Standless turtle

--- src/main/java/com/github/standobyte/jojo/entity/mob/CocoJumboTurtleEntity.java
+++ src/main/java/com/github/standobyte/jojo/entity/mob/CocoJumboTurtleEntity.java
@@ -5,6 +5,7 @@ import com.github.standobyte.jojo.action.ActionConditionResult;
 import com.github.standobyte.jojo.advancements.ModCriteriaTriggers;
 import com.github.standobyte.jojo.client.ClientUtil;
 import com.github.standobyte.jojo.client.render.item.InventoryItemHighlight;
+import com.github.standobyte.jojo.client.ui.actionshud.ActionsOverlayGui;
 import com.github.standobyte.jojo.init.ModEntityTypes;
 import com.github.standobyte.jojo.init.ModItems;
 import com.github.standobyte.jojo.init.ModStatusEffects;
@@ -93,7 +94,8 @@ public class CocoJumboTurtleEntity extends TurtleEntity implements IMobStandUser
                 }
             }
         }
-        else if (!standPower.hasPower() && !this.hasEffect(ModStatusEffects.STAND_VIRUS.get())) {
+        else if (ActionsOverlayGui.getInstance().getMouseTarget().getEntity() == this
+                && !standPower.hasPower() && !this.hasEffect(ModStatusEffects.STAND_VIRUS.get())) {
             if (ClientUtil.getClientPlayer().distanceToSqr(this) < 36) {
                 InventoryItemHighlight.highlightItem(Items.BOW, 20);
                 InventoryItemHighlight.highlightItem(Items.CROSSBOW, 20);
