c14abb0e32279ae0c75e0f472d5f8d5b8f53fd0d
StandoByte <94539965+StandoByte@users.noreply.github.com>
2024-06-30 22:20:17 +0300

    Rendering off-hand gloves in first person

--- src/main/java/com/github/standobyte/jojo/client/ClientEventHandler.java
+++ src/main/java/com/github/standobyte/jojo/client/ClientEventHandler.java
@@ -585,9 +585,11 @@ public class ClientEventHandler {
                                     event.getPartialTicks(), event.getInterpolatedPitch(), player);
                         }
                     });
-                    
-                    if (GlovesLayer.areGloves(item) || item.isEmpty() && !player.isInvisible() && 
-                            (player.hasEffect(ModStatusEffects.HAMON_SPREAD.get()) || player.hasEffect(ModStatusEffects.FREEZE.get()))) {
+
+                    boolean hasGloves = GlovesLayer.areGloves(player.getItemInHand(Hand.MAIN_HAND)) || GlovesLayer.areGloves(player.getItemInHand(Hand.OFF_HAND));
+                    boolean hasEffect = player.hasEffect(ModStatusEffects.HAMON_SPREAD.get()) || player.hasEffect(ModStatusEffects.FREEZE.get());
+                    if (hasGloves && (GlovesLayer.areGloves(item) || item.isEmpty()) || 
+                            hasEffect && item.isEmpty() && !player.isInvisible()) {
                         event.setCanceled(true);
                         renderHand(Hand.MAIN_HAND, event.getMatrixStack(), event.getBuffers(), event.getLight(), 
                                 event.getPartialTicks(), event.getInterpolatedPitch(), player);
