8b648e430055369c85db6eb223000a55d62ffe6d
StandoByte <94539965+StandoByte@users.noreply.github.com>
2024-09-12 01:10:31 +0300

    Made power type arguments more convenient with addons

--- src/main/java/com/github/standobyte/jojo/command/argument/NonStandTypeArgument.java
+++ src/main/java/com/github/standobyte/jojo/command/argument/NonStandTypeArgument.java
@@ -2,13 +2,8 @@ package com.github.standobyte.jojo.command.argument;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Locale;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.stream.Stream;
 
-import com.github.standobyte.jojo.JojoMod;
 import com.github.standobyte.jojo.init.power.JojoCustomRegistries;
 import com.github.standobyte.jojo.power.impl.nonstand.type.NonStandPowerType;
 import com.mojang.brigadier.StringReader;
@@ -16,14 +11,11 @@ import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
-import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 
 import net.minecraft.command.arguments.ArgumentSerializer;
 import net.minecraft.command.arguments.ArgumentTypes;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.ResourceLocationException;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
@@ -37,37 +29,9 @@ public class NonStandTypeArgument implements ArgumentType<NonStandPowerType<?>> 
     
     @Override
     public NonStandPowerType<?> parse(StringReader reader) throws CommandSyntaxException {
-        ResourceLocation resourceLocation = read(reader);
-        NonStandPowerType<?> powerType = JojoCustomRegistries.NON_STAND_POWERS.getRegistry().getValue(resourceLocation);
-        if (powerType == null) {
-            throw TYPE_UNKNOWN.create(resourceLocation);
-        }
-        return powerType;
+        String input = StandArgument.read(reader);
+        return StandArgument.bestFittingValue(input, JojoCustomRegistries.NON_STAND_POWERS.getRegistry(), TYPE_UNKNOWN);
     }
-
-    private static ResourceLocation read(StringReader reader) throws CommandSyntaxException {
-        int i = reader.getCursor();
-        while(reader.canRead() && isValidPathCharacter(reader.peek())) {
-            reader.skip();
-        }
-        String s = reader.getString().substring(i, reader.getCursor());
-        try {
-            return modResourceLocation(s);
-        } catch (ResourceLocationException var4) {
-            reader.setCursor(i);
-            throw INVALID_EXCEPTION.createWithContext(reader);
-        }
-    }
-
-    private static boolean isValidPathCharacter(char charIn) {
-        return charIn >= '0' && charIn <= '9' || charIn >= 'a' && charIn <= 'z' || charIn == '_' || charIn == ':' || charIn == '/' || charIn == '.' || charIn == '-';
-    }
-
-    private static ResourceLocation modResourceLocation(String s) {
-        return s.contains(":") ? new ResourceLocation(s) : new ResourceLocation(JojoMod.MOD_ID, s);
-    }
-
-    private static final SimpleCommandExceptionType INVALID_EXCEPTION = new SimpleCommandExceptionType(new TranslationTextComponent("argument.id.invalid"));
 
     public static <S> NonStandPowerType<?> getPowerType(CommandContext<S> context, String name) {
         return context.getArgument(name, NonStandPowerType.class);
@@ -75,31 +39,10 @@ public class NonStandTypeArgument implements ArgumentType<NonStandPowerType<?>> 
 
     @Override
     public <S> CompletableFuture<Suggestions> listSuggestions(final CommandContext<S> context, final SuggestionsBuilder builder) {
-        return suggestIterable(JojoCustomRegistries.NON_STAND_POWERS.getRegistry().getValues()
+        return StandArgument.suggestIterable(JojoCustomRegistries.NON_STAND_POWERS.getRegistry().getValues()
                 .stream().map(IForgeRegistryEntry::getRegistryName), builder);
     }
-
-    private static CompletableFuture<Suggestions> suggestIterable(Stream<ResourceLocation> registryKeys, SuggestionsBuilder builder) {
-        String s = builder.getRemaining().toLowerCase(Locale.ROOT);
-        addSuggestions(registryKeys, s, (resLoc) -> resLoc, (resLoc) -> builder.suggest(resLoc.toString()));
-        return builder.buildFuture();
-    }
-
-    private static <T> void addSuggestions(Stream<T> stream, String remaining, Function<T, ResourceLocation> toResLoc, Consumer<T> suggest) {
-        boolean inputColon = remaining.indexOf(':') > -1;
-        stream.forEach(t -> {
-            ResourceLocation resourceLocation = toResLoc.apply(t);
-            if (inputColon) {
-                String s = resourceLocation.toString();
-                if (s.startsWith(remaining)) {
-                    suggest.accept(t);
-                }
-            } else if (resourceLocation.getNamespace().startsWith(remaining) || resourceLocation.getNamespace().equals(JojoMod.MOD_ID) && resourceLocation.getPath().startsWith(remaining)) {
-                suggest.accept(t);
-            }
-        });
-    }
-
+    
     @Override
     public Collection<String> getExamples() {
         return EXAMPLES;
