0a6036740d70e9e9a8eefcaff77b5b8e3adb357c
StandoByte <94539965+StandoByte@users.noreply.github.com>
2024-09-17 15:53:08 +0300

    Improved action argument suggestions

--- src/main/java/com/github/standobyte/jojo/command/argument/NonStandTypeArgument.java
+++ src/main/java/com/github/standobyte/jojo/command/argument/NonStandTypeArgument.java
@@ -18,24 +18,23 @@ import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 
 import net.minecraft.command.arguments.ArgumentSerializer;
 import net.minecraft.command.arguments.ArgumentTypes;
-import net.minecraft.util.RegistryKey;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
 public class NonStandTypeArgument implements ArgumentType<NonStandPowerType<?>> {
     private static final Collection<String> EXAMPLES = Arrays.asList("hamon", "vampirism");
     public static final DynamicCommandExceptionType TYPE_UNKNOWN = new DynamicCommandExceptionType((key) -> new TranslationTextComponent("non_stand.unknown", key));
-    private static Map<String, List<Map.Entry<RegistryKey<NonStandPowerType<?>>, NonStandPowerType<?>>>> BY_LOCATION;
+    private static Map<String, List<NonStandPowerType<?>>> BY_LOCATION;
     
     public static void commonSetupRegister() {
         ArgumentTypes.register("non_stand", NonStandTypeArgument.class, new ArgumentSerializer<>(NonStandTypeArgument::new));
-        BY_LOCATION = StandArgument.groupByKeyLocation(JojoCustomRegistries.NON_STAND_POWERS.getRegistry());
+        BY_LOCATION = ArgumentUtil.groupByKeyLocation(JojoCustomRegistries.NON_STAND_POWERS.getRegistry());
     }
     
     @Override
     public NonStandPowerType<?> parse(StringReader reader) throws CommandSyntaxException {
-        String input = StandArgument.read(reader);
-        return StandArgument.bestFittingValue(input, JojoCustomRegistries.NON_STAND_POWERS.getRegistry(), BY_LOCATION, TYPE_UNKNOWN);
+        String input = ArgumentUtil.read(reader);
+        return ArgumentUtil.bestFittingValue(input, JojoCustomRegistries.NON_STAND_POWERS.getRegistry(), BY_LOCATION, TYPE_UNKNOWN);
     }
     
     public static <S> NonStandPowerType<?> getPowerType(CommandContext<S> context, String name) {
@@ -44,7 +43,7 @@ public class NonStandTypeArgument implements ArgumentType<NonStandPowerType<?>> 
     
     @Override
     public <S> CompletableFuture<Suggestions> listSuggestions(final CommandContext<S> context, final SuggestionsBuilder builder) {
-        return StandArgument.suggestIterable(JojoCustomRegistries.NON_STAND_POWERS.getRegistry().getValues()
+        return ArgumentUtil.suggestIterable(JojoCustomRegistries.NON_STAND_POWERS.getRegistry().getValues()
                 .stream().map(IForgeRegistryEntry::getRegistryName), builder);
     }
     
