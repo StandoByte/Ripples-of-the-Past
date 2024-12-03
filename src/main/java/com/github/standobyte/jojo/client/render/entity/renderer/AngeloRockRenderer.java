fc5459007c4d0424d737254ad9ea5a4e0f3f1f11
StandoByte <94539965+StandoByte@users.noreply.github.com>
2024-12-03 23:43:35 +0300

    Angelo rock model by August_dr

--- src/main/java/com/github/standobyte/jojo/client/render/entity/renderer/AngeloRockRenderer.java
+++ src/main/java/com/github/standobyte/jojo/client/render/entity/renderer/AngeloRockRenderer.java
@@ -1,5 +1,6 @@
 package com.github.standobyte.jojo.client.render.entity.renderer;
 
+import com.github.standobyte.jojo.JojoMod;
 import com.github.standobyte.jojo.client.render.entity.model.AngeloRockModel;
 import com.github.standobyte.jojo.client.render.entity.renderer.damaging.projectile.CDBlockBulletRenderer;
 import com.github.standobyte.jojo.entity.AngeloRockEntity;
@@ -8,12 +9,14 @@ import com.mojang.blaze3d.vertex.IVertexBuilder;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.ResourceLocation;
 
 public class AngeloRockRenderer extends SimpleEntityRenderer<AngeloRockEntity, AngeloRockModel> {
     public static final ResourceLocation TEXTURE = new ResourceLocation("textures/block/stone.png");
+    public static final ResourceLocation SHADOW_TEXTURE = new ResourceLocation(JojoMod.MOD_ID, "textures/entity/angelo_rock_shadow.png");
 
     public AngeloRockRenderer(EntityRendererManager renderManager) {
         super(renderManager, new AngeloRockModel(), TEXTURE);
@@ -21,6 +24,7 @@ public class AngeloRockRenderer extends SimpleEntityRenderer<AngeloRockEntity, A
     
     @Override
     protected void doRender(AngeloRockEntity entity, AngeloRockModel model, float partialTick, MatrixStack matrixStack, IRenderTypeBuffer buffer, int packedLight) {
+        model.shadow.visible = false;
         BlockState blockUpper = entity.getUpperBlock();
         BlockState blockLower = entity.getLowerBlock();
         if (blockUpper.equals(blockLower)) {
@@ -35,6 +39,14 @@ public class AngeloRockRenderer extends SimpleEntityRenderer<AngeloRockEntity, A
             model.upperHalf.visible = false;
             model.lowerHalf.visible = true;
             renderRockPart(entity, blockLower, model, partialTick, matrixStack, buffer, packedLight);
+        }
+        
+        if (entity.getCreationAnimProgress(partialTick) >= 1) {
+            model.shadow.visible = true;
+            model.upperHalf.visible = false;
+            model.lowerHalf.visible = false;
+            IVertexBuilder vertexBuilder = buffer.getBuffer(RenderType.entityTranslucent(SHADOW_TEXTURE));
+            model.renderToBuffer(matrixStack, vertexBuilder, packedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
         }
     }
     
