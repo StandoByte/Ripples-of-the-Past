4936240fcf7be8d1b1aba29e5768f05bf8610a1e
StandoByte <94539965+StandoByte@users.noreply.github.com>
2025-02-01 01:41:30 +0300

    One more fix to mesh faces

--- src/main/java/com/github/standobyte/jojo/client/render/entity/bb/ParseGenericModel.java
+++ src/main/java/com/github/standobyte/jojo/client/render/entity/bb/ParseGenericModel.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 import javax.annotation.Nullable;
@@ -147,6 +149,7 @@ public class ParseGenericModel {
                 int texture;
             }
 
+            private static final Set<String> visitedVertices = new LinkedHashSet<>(4);
             @Override
             void addElement(ModelRenderer parent, ModelParsed.GroupParsed parentParsed, 
                     List<ModelRenderer.ModelBox> modelCubesCollection, int texWidth, int texHeight) {
@@ -157,11 +160,15 @@ public class ParseGenericModel {
                 for (Map.Entry<String, MeshFace> meshFace : faces.entrySet()) {
                     MeshFace face = meshFace.getValue();
                     if (face.vertices.length > 2) {
-                        // FIXME mesh face normal (cross product)
-                        Vertex[] verticesArr = new Vertex[face.vertices.length];
-                        for (int i = 0; i < verticesArr.length; ++i) {
-                            String vertexId = face.vertices[i];
-                            verticesArr[i] = new Vertex(vertices.get(vertexId), face.uv.get(vertexId));
+                        visitedVertices.clear();
+                        for (String vertex : face.vertices) {
+                            // using an ordered set in case a mesh face uses the same vertex twice for whatever f-ing reason
+                            visitedVertices.add(vertex);
+                        }
+                        Vertex[] verticesArr = new Vertex[visitedVertices.size()];
+                        int i = 0;
+                        for (String vertexId : visitedVertices) {
+                            verticesArr[i++] = new Vertex(vertices.get(vertexId), face.uv.get(vertexId));
                         }
                         sortVertices(verticesArr);
                         
