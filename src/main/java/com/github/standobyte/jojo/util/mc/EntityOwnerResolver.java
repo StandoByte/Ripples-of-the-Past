23c2e0723fb49fdefa4fde93415b194c0f8265df
StandoByte <94539965+StandoByte@users.noreply.github.com>
2025-03-16 00:04:37 +0300

    EntityOwnerResolver backport

--- src/main/java/com/github/standobyte/jojo/util/mc/EntityOwnerResolver.java
+++ src/main/java/com/github/standobyte/jojo/util/mc/EntityOwnerResolver.java
@@ -12,10 +12,10 @@ import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
 public class EntityOwnerResolver {
-    private Entity owner;
-    private LivingEntity ownerLiving;
-    private UUID ownerUUID;
-    private int ownerNetworkId;
+    protected Entity owner;
+    protected LivingEntity ownerLiving;
+    protected UUID ownerUUID;
+    protected int ownerNetworkId;
     
     public void setOwner(@Nullable Entity owner) {
         this.ownerUUID = owner != null ? owner.getUUID() : null;
@@ -23,7 +23,7 @@ public class EntityOwnerResolver {
         _setNewOwnerEntity(owner);
     }
     
-    public void setOwner(UUID ownerUuid) {
+    public void setOwnerUUID(UUID ownerUuid) {
         this.ownerUUID = ownerUuid;
     }
     
@@ -69,7 +69,7 @@ public class EntityOwnerResolver {
     }
     
     public void loadNbt(CompoundNBT nbt, String key) {
-        setOwner(nbt.hasUUID(key) ? nbt.getUUID(key) : null);
+        setOwnerUUID(nbt.hasUUID(key) ? nbt.getUUID(key) : null);
     }
     
     public void writeNetwork(PacketBuffer buf) {
@@ -82,5 +82,26 @@ public class EntityOwnerResolver {
     
     public int getNetworkId() {
         return ownerNetworkId;
+    }
+    
+    
+    public static class Generic<T extends Entity> extends EntityOwnerResolver {
+        protected final Class<T> entityClass;
+        protected T castEntity;
+        
+        public Generic(Class<T> entityClass) {
+            this.entityClass = entityClass;
+        }
+        
+        public T getEntityCast(World world) {
+            updateEntity(world);
+            return castEntity;
+        }
+        
+        @Override
+        protected void _setNewOwnerEntity(Entity entity) {
+            super._setNewOwnerEntity(entity);
+            this.castEntity = entity != null && entityClass.isAssignableFrom(entity.getClass()) ? (T) entity : null;
+        }
     }
 }
