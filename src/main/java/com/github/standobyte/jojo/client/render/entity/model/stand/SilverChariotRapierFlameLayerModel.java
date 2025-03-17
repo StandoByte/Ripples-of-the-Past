309a55332541b3bd42e0f0a1d33190938da535a7
StandoByte <94539965+StandoByte@users.noreply.github.com>
2025-03-18 00:48:14 +0300

    Fixed SC rapier flame rendering

--- src/main/java/com/github/standobyte/jojo/client/render/entity/model/stand/SilverChariotRapierFlameLayerModel.java
+++ src/main/java/com/github/standobyte/jojo/client/render/entity/model/stand/SilverChariotRapierFlameLayerModel.java
@@ -1,29 +1,29 @@
 package com.github.standobyte.jojo.client.render.entity.model.stand;
 
 import com.github.standobyte.jojo.client.render.FlameModelRenderer;
-import com.github.standobyte.jojo.entity.stand.stands.SilverChariotEntity;
 
-import net.minecraft.client.renderer.Atlases;
-import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.util.Direction;
 
 public class SilverChariotRapierFlameLayerModel extends SilverChariotModel {
     private FlameModelRenderer rapierBladeFlame;
 
     public SilverChariotRapierFlameLayerModel() {
-//        super(tex -> Atlases.translucentCullBlockSheet(), 64, 64);
+        super();
+        texWidth = 64;
+        texHeight = 64;
         
-//        rapierBladeFlame = new FlameModelRenderer(this);
-//        rapierBladeFlame.setPos(0.0F, 0.0F, 0.0F);
-//        rapier.addChild(rapierBladeFlame);
-//        rapierBladeFlame.addFlame(0, 0, -3.0F, 1, 3, Direction.NORTH);
-//        rapierBladeFlame.addFlame(0, 0, -4.5F, 1, 3, Direction.NORTH);
-//        rapierBladeFlame.addFlame(0, 0, -6.0F, 1, 3, Direction.NORTH);
-//        rapierBladeFlame.addFlame(0, 0, -7.5F, 1, 3, Direction.NORTH);
-//        rapierBladeFlame.addFlame(0, 0, -9.0F, 1, 3, Direction.NORTH);
-//        rapierBladeFlame.addFlame(0, 0, -10.5F, 1, 3, Direction.NORTH);
-//        rapierBladeFlame.addFlame(0, 0, -12.0F, 1, 3, Direction.NORTH);
-//        rapierBladeFlame.addFlame(0, 0, -13.5F, 1, 3, Direction.NORTH);
+        clearAllCubes();
+        rapierBladeFlame = new FlameModelRenderer(this);
+        rapierBladeFlame.setPos(0.0F, 0.0F, 0.0F);
+        rapier.addChild(rapierBladeFlame);
+        rapierBladeFlame.addFlame(0, 0, -3.0F, 1, 3, Direction.NORTH);
+        rapierBladeFlame.addFlame(0, 0, -4.5F, 1, 3, Direction.NORTH);
+        rapierBladeFlame.addFlame(0, 0, -6.0F, 1, 3, Direction.NORTH);
+        rapierBladeFlame.addFlame(0, 0, -7.5F, 1, 3, Direction.NORTH);
+        rapierBladeFlame.addFlame(0, 0, -9.0F, 1, 3, Direction.NORTH);
+        rapierBladeFlame.addFlame(0, 0, -10.5F, 1, 3, Direction.NORTH);
+        rapierBladeFlame.addFlame(0, 0, -12.0F, 1, 3, Direction.NORTH);
+        rapierBladeFlame.addFlame(0, 0, -13.5F, 1, 3, Direction.NORTH);
     }
 
 }
